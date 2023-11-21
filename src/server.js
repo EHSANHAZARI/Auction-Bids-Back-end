e14522e781df64fb59527126e75a6f2059ef6b8b
Ehsan <esihazar@gmail.com>
2023-11-21 09:50:34 -0500

    backend added connected to database using monogoose

---
+++ src/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8000;
+const mongoose = require("mongoose");
+
+const dbUrl =
+  "mongodb+srv://Auction:DaWcvAENC9JnSPbi@cluster0.tzzkwds.mongodb.net/?retryWrites=true&w=majority";
+const connectionParams = {
+  useNewUrlParser: true, // Corrected typo here: 'useNewUrlPaser' to 'useNewUrlParser'
+  useUnifiedTopology: true,
+};
+
+mongoose
+  .connect(dbUrl, connectionParams)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((error) => {
+    // Corrected the syntax here: 'error;' to '(error) => { console.log(error); }'
+    console.log(error);
+  });
+
+app.get("/hello", (req, res) => {
+  res.send("Hello from the backend!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
